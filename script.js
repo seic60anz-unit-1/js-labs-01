a7aa567b2d2d7dc689c921b44ec03127ec27ec9e
Penelope Chen <pen.c828@gmail.com>
2022-12-07 16:12:56 +1100

    2nd exercise done

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 // Every 2
-for (let i = 0; i < 10; i+=2) {
-    console.log(i)
-}
+// for (let i = 0; i < 10; i+=2) {
+//     console.log(i)
+// }
 
 // 9 to 3
+// for (let i = 9; i > 2; i--) {
+//     console.log(i)
+// }
+
+// 9 Times Table
