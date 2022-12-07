8eb089bcda178c6c6eb9da9ed92a4ed5e7575e0d
Wing Fung Lau <wingfunglau@Wings-MacBook-Pro.local>
2022-12-07 15:54:35 +1100

    9 to 3

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 //Every 2
 
-for (let i = 2; i <= 10; i = i+2) {
+// for (let i = 2; i <= 10; i = i+2) {
+//     console.log(i)
+// }
+
+//9 to 3
+for (let i = 9;i >= 3; i--) {
     console.log(i)
 }
