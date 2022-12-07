eb322035f0a6e5a32d276f4deacdc542f6962487
HueNguyen <nguyenhue.se@gmail.com>
2022-12-07 16:14:43 +1100

    submit wk01d03 lab2

---
+++ script.js
@@ -0,0 +1,30 @@
+// // Every 2
+// for (let i = 0; i < 100; i += 2) {
+//     console.log(i);
+// }
+
+// // 9 to 3
+// for (let i = 9; i > 0; i -= 3) {
+//     console.log(i);
+// };
+
+// 9 times table
+const nineTimesTable = function () {
+    let lines = ``
+    for (let i = 0; i <= 10; i++) {
+        if (i <= 9) {
+            lines += `9 times ${i} is ${9 * i}\n`
+        } else {
+            lines += `9 times ${i} is ${9 * i}`
+        }
+
+    }
+    return `
+9 Times Table
+================
+${lines}
+================
+`;
+}
+
+console.log(nineTimesTable());
