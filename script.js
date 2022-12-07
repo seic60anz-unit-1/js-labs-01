afa1b0445e829ab7589be4118641b8ed626f9a56
JP <jpcodingjourney@JPs-MacBook-Air.local>
2022-12-07 15:59:54 +1100

    w1d3 loops classwork

---
+++ script.js
@@ -0,0 +1,24 @@
+// Every 2
+// for (let i = 0; i <= 20; i+=2) {
+//     console.log(i)
+// }
+
+// 9 to 3
+// for (let i = 9; i > 2; i--) {
+    
+//     console.log(i)
+// }
+
+// 9 Times Table
+// for (let i = 0; i <= 10; i++) {
+//     console.log(`9 times ${i} is ${9*i}`)
+// }
+
+// Even/Odd Reporter
+// for (let i = 1; i <= 20; i++) {
+//     if (i % 2 === 0) {
+//         console.log(`${i} is even`)
+//     } else {
+//         console.log(`${i} is odd`)
+//     }
+// }
