8abe80aec7a4cb373fef5fbf1f55b7c39c1886bb
Wing Fung Lau <wingfunglau@Wings-MacBook-Pro.local>
2022-12-07 15:58:06 +1100

    9 Times Table

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
 //Every 2
 
-// for (let i = 2; i <= 10; i = i+2) {
+// for (let i = 2; i <= 10; i += 2) {
 //     console.log(i)
 // }
 
 //9 to 3
-for (let i = 9;i >= 3; i--) {
-    console.log(i)
+
+// for (let i = 9;i >= 3; i--) {
+//     console.log(i)
+// }
+
+//9 Times Table
+
+console.log("9 Times Table")
+console.log("================")
+
+for (let i = 0; i < 11; i++) {
+    console.log(`9 times ${i} is ${i * 9}`)
 }
+console.log("================")
